39858177a0c980954e38b1b37a0126299fbce65f
Edmundo Sanchez <e.sanchez@eyeo.com>
2024-11-27 11:34:56 -0600

    run black on test fitlers blocks

--- tests/test_filters_blocks.py
+++ tests/test_filters_blocks.py
@@ -25,18 +25,18 @@ import pytest
 from abp.filters import parse_filterlist, SelectorType, FilterAction
 from abp.filters.blocks import to_blocks
 
-DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
 
 
 @pytest.fixture()
 def fl_lines():
-    with open(os.path.join(DATA_PATH, 'filterlist.txt')) as f:
+    with open(os.path.join(DATA_PATH, "filterlist.txt")) as f:
         return list(parse_filterlist(f))
 
 
 @pytest.fixture()
 def expected_blocks():
-    with open(os.path.join(DATA_PATH, 'expected_blocks.json')) as f:
+    with open(os.path.join(DATA_PATH, "expected_blocks.json")) as f:
         return json.load(f)
 
 
@@ -44,14 +44,18 @@ def test_to_blocks(fl_lines):
     blocks = list(to_blocks(fl_lines))
     assert len(blocks) == 3
     block = blocks[0]
-    assert block.variables['foo'] == 'bar'
-    assert block.variables['baz'] == ('some_tricky?variable=with&funny=chars#'
-                                      'and-stuff')
+    assert block.variables["foo"] == "bar"
+    assert block.variables["baz"] == (
+        "some_tricky?variable=with&funny=chars#" "and-stuff"
+    )
     print(block.description)
-    assert block.description == 'Example block 1\nAnother comment\nAnother block\nSnippet filters with non-ascii characters'
+    assert (
+        block.description
+        == "Example block 1\nAnother comment\nAnother block\nSnippet filters with non-ascii characters"
+    )
     # Don't test the filters thouroughly: filter parsing is tested elsewhere.
     assert len(block.filters) == 5
-    assert block.filters[0].selector['type'] == SelectorType.URL_PATTERN
+    assert block.filters[0].selector["type"] == SelectorType.URL_PATTERN
     assert block.filters[1].action == FilterAction.SHOW
 
 
